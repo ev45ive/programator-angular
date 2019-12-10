cfa78ed5356aa9e12cba61f401c4da7c22fd765b
Your Name <you@example.com>
2019-12-10 20:08:20 +0100

    ngFor, ngForOf, let-zmienna = implicit

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { NgForOf, NgForOfContext } from '@angular/common';
+
+NgForOf
+NgForOfContext
 
 @Component({
   selector: "app-playlist, .placki-playlist",
   templateUrl: "./playlist.component.html",
   styleUrls: ["./playlist.component.scss"]
 })
-
 export class PlaylistComponent {
   mode = "show";
+
+  playlists: Playlist[] = [
+    {
+      id: 123,
+      name: "Placki 123",
+      color: "#ffff00",
+      favorite: false
+    },
+    {
+      id: 234,
+      name: "Placki 234",
+      color: "#ff00ff",
+      favorite: true
+    },
+    {
+      id: 345,
+      name: "Placki 345",
+      color: "#ff00ff",
+      favorite: false
+    },
+  ];
 
   playlist: Playlist = {
     id: 123,
@@ -19,12 +43,12 @@ export class PlaylistComponent {
   edit() {
     this.mode = "edit";
   }
-  
+
   cancel() {
     this.mode = "show";
   }
 
-  constructor(cdr:ChangeDetectorRef) {
+  constructor(cdr: ChangeDetectorRef) {
     // cdr.detectChanges()
   }
 
